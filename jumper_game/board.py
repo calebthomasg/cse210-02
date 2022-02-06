ff06c888562d54d9f672bb83e3f9bb1b1d6ce79a
erinstrydom21@gmail.com <erinstrydom21@gmail.com>
2022-02-06 13:46:35 +0200

    added comments

--- jumper_game/board.py
+++ jumper_game/board.py
@@ -1,4 +1,8 @@
 def board(self):
+    # This is just a basic idea of how the board will look at the very beginning of the game.
+    # I'm not sure if we want this to be a class or if we should just leave it as a function and
+    # add it to an existing class.
+
     if self._is_playing == True:
         print("_ _ _ _ _")
         print("  ___  ")
--- jumper_game/word.py
+++ jumper_game/word.py
@@ -7,6 +7,10 @@ class Word:
         self.value = ""
 
     def choose_word(self):
+        # This is a list of five letter words.
+        # The self.value variable will randomly be assigned a word from the list.
+        # We'll also need to somehow divide this word into individual letters.
+        # The split() function could do this.
 
         words = ["clown", "lunch", "towel", "faith", "scowl", "cloth", "video", "piano", "horse", "stone"]
         self.value = random.choice(words)

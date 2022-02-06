00c8cf1e16250ede5df8494a63057854a50527aa
erinstrydom21@gmail.com <erinstrydom21@gmail.com>
2022-02-06 13:35:31 +0200

    creation of board

---
+++ jumper_game/board.py
@@ -0,0 +1,11 @@
+def board(self):
+    if self._is_playing == True:
+        print("_ _ _ _ _")
+        print("  ___  ")
+        print(" /___\ ")
+        print(" \   / ")
+        print("  \ /  ")
+        print("   O   ")
+        print("  /|\  ")
+        print("  / \  ")
+        print("\n^^^^^^^")
